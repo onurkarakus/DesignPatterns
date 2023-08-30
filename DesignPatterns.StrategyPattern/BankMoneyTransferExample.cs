69fa64f714934b2c036f2ddfe2cd3f2c0c3f1d62
Onur KARAKUŞ <e_nigmais@hotmail.com>
2023-08-30 13:43:00 +0300

    Add project files.

---
+++ DesignPatterns.StrategyPattern/BankMoneyTransferExample.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.StrategyPattern
+{
+    // Transfer Money With Different Options
+
+    public interface ITransferMoney
+    {
+        void TransferMoney(PaymentOptions options);
+    }
+
+    public class PaymentOptions
+    {
+        public string SourceAccount { get; set; }
+        public string DestinationAccount { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime TransferDate { get; set; }
+    }
+
+    public class BankTransfer : ITransferMoney
+    {
+        public void TransferMoney(PaymentOptions options)
+        {
+            Console.WriteLine($"Transfering money with bank transfer " +
+                $"{options.SourceAccount} " +
+                $"To {options.DestinationAccount} " +
+                $"At {options.TransferDate.ToShortDateString()} " +
+                $"With Amount : {options.Amount}");
+        }
+    }
+
+    public class PayPalTransfer : ITransferMoney
+    {
+        public void TransferMoney(PaymentOptions options)
+        {
+            Console.WriteLine($"Transfering money with PayPal " +
+                $"{options.SourceAccount} " +
+                $"To {options.DestinationAccount} " +
+                $"At {options.TransferDate.ToShortDateString()} " +
+                $"With Amount : {options.Amount}");
+        }
+    }
+
+    public class FastTransfer : ITransferMoney
+    {
+        public void TransferMoney(PaymentOptions options)
+        {
+            Console.WriteLine($"Transfering money with Fast " +
+                $"{options.SourceAccount} " +
+                $"To {options.DestinationAccount} " +
+                $"At {options.TransferDate.ToShortDateString()} " +
+                $"With Amount : {options.Amount}");
+        }
+    }
+
+    public class MoneyTransfer
+    {
+        private ITransferMoney _transferMoney;
+
+        public MoneyTransfer(ITransferMoney transferMoney)
+        {
+            _transferMoney = transferMoney;
+        }
+
+        public MoneyTransfer() { }
+
+        public void SetAttackStrategy(ITransferMoney transferMoney)
+        {
+            _transferMoney = transferMoney;
+        }
+
+        public void TransferMoney(PaymentOptions options)
+        {
+            _transferMoney.TransferMoney(options);
+        }
+    }
+
+    public static class BankMoneyTransferExample
+    {
+        public static void RunSample()
+        {
+            var newMoneyTransfer = new MoneyTransfer(new BankTransfer());
+
+            newMoneyTransfer.TransferMoney(new PaymentOptions
+            {
+                Amount = 100,
+                DestinationAccount = "123456789",
+                SourceAccount = "987654321",
+                TransferDate = DateTime.Now
+            });
+
+        }
+    }
+}
---
+++ DesignPatterns.StrategyPattern/FootballExample.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.StrategyPattern
+{
+    public interface IAttackStrategy
+    {
+        void ApplyStrategy();
+    }
+
+    public class OffensiveStrategy : IAttackStrategy
+    {
+        public void ApplyStrategy()
+        {
+            Console.WriteLine("Attacking offensively");
+        }
+    }
+
+    public class DefensiveStrategy : IAttackStrategy
+    {
+        public void ApplyStrategy()
+        {
+            Console.WriteLine("Attacking defensively");
+        }
+    }
+
+    public class MyTeam
+    {
+        private IAttackStrategy _attackStrategy;
+
+        public MyTeam(IAttackStrategy attackStrategy)
+        {
+            _attackStrategy = attackStrategy;
+        }
+
+        public MyTeam()
+        {
+
+        }
+
+        public void SetAttackStrategy(IAttackStrategy attackStrategy)
+        {
+            _attackStrategy = attackStrategy;
+        }
+
+        public void ApplyAttackStrategy()
+        {
+            _attackStrategy.ApplyStrategy();
+        }
+    }
+
+    public static class FootballExample
+    {
+        public static void RunSample()
+        {
+            var myTeam = new MyTeam(new OffensiveStrategy());
+            myTeam.ApplyAttackStrategy();
+
+            myTeam.SetAttackStrategy(new DefensiveStrategy());
+            myTeam.ApplyAttackStrategy();
+        }
+    }
+}

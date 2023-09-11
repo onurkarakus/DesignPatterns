501f005654bbe9813e8bd8bf272e94f9d6f2d5fc
Onur KARAKUŞ <e_nigmais@hotmail.com>
2023-09-12 02:34:11 +0300

    Add Factory Pattern Example

---
+++ DesignPatterns.FactoryPattern/CreditCardExample.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.FactoryPattern;
+
+public static class CreditCardExample
+{
+    public interface ICreditCard
+    {
+        string GetCardType();
+        int GetCreditLimit();
+        int GetAnnualCharge();
+    }
+
+    public abstract class CreditCardFactory
+    {
+        protected abstract ICreditCard MakeProduct();
+
+        public string CreateProduct()
+        {
+            var creditCard = this.MakeProduct();
+
+            var result = $"Card Type : {creditCard.GetCardType()} / Card Limit : {creditCard.GetCreditLimit()} / Annual Charge: {creditCard.GetAnnualCharge()}";
+
+            return result;
+        }
+    }
+
+    public class MoneyBackFactory : CreditCardFactory
+    {
+        protected override ICreditCard MakeProduct()
+        {
+            return new MoneyBack();
+        }
+    }
+
+    public class TitaniumFactory : CreditCardFactory
+    {
+        protected override ICreditCard MakeProduct()
+        {
+            return new Titanium();
+        }
+    }
+
+    public class PlatinumFactory : CreditCardFactory
+    {
+        protected override ICreditCard MakeProduct()
+        {
+            return new Platinum();
+        }
+    }
+
+    public class MoneyBack : ICreditCard
+    {
+        public int GetAnnualCharge()
+        {
+            return 500;
+        }
+
+        public string GetCardType()
+        {
+            return "MoneyBack";
+        }
+
+        public int GetCreditLimit()
+        {
+            return 15000;
+        }
+    }
+
+    public class Titanium : ICreditCard
+    {
+        public int GetAnnualCharge()
+        {
+            return 1500;
+        }
+
+        public string GetCardType()
+        {
+            return "Titanium Edge";
+        }
+
+        public int GetCreditLimit()
+        {
+            return 25000;
+        }
+    }
+
+    public class Platinum : ICreditCard
+    {
+        public int GetAnnualCharge()
+        {
+            return 2000;
+        }
+
+        public string GetCardType()
+        {
+            return "Platinum Plus";
+        }
+
+        public int GetCreditLimit()
+        {
+            return 35000;
+        }
+    }
+
+    public static void RunSample()
+    {
+        var moneyBackFactory = new MoneyBackFactory();
+        var titaniumFactory = new TitaniumFactory();
+        var platinumFactory = new PlatinumFactory();
+
+        Console.WriteLine(moneyBackFactory.CreateProduct());
+        Console.WriteLine(titaniumFactory.CreateProduct());
+        Console.WriteLine(platinumFactory.CreateProduct());
+    }
+
+
+
+}
